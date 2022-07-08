9a6b71aed07fa9deaa1d0cfbd812d049daecd1e1
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2022-07-08 09:11:51 +0600

    Create file-picker.php

---
+++ file-picker.php
@@ -0,0 +1,26 @@
+<?php
+
+function file_picker_settings_field($settings, $value)
+{
+	$output = '';
+	$select_file_class = '';
+	$remove_file_class = ' hidden';
+	$attachment_url = wp_get_attachment_url($value);
+	if ($attachment_url) {
+		$select_file_class = ' hidden';
+		$remove_file_class = '';
+	}
+	$output .= '<div class="file_picker_block">
+				  <div class="' . esc_attr($settings['type']) . '_display">' .
+		$attachment_url .
+		'</div>
+				  <input type="hidden" name="' . esc_attr($settings['param_name']) . '" class="wpb_vc_param_value wpb-textinput ' .
+		esc_attr($settings['param_name']) . ' ' .
+		esc_attr($settings['type']) . '_field" value="' . esc_attr($value) . '" />
+				  <button class="button file-picker-button' . $select_file_class . '">Select File</button>
+				  <button class="button file-remover-button' . $remove_file_class . '">Remove File</button>
+				</div>
+				';
+	return $output;
+}
+vc_add_shortcode_param('file_picker', 'file_picker_settings_field', get_template_directory_uri() . '/inc/wp-bakery-file-picker/file_picker.js');
